c4dbd53954ad03cc092693b196531538ad9a6baa
Matias Labra <gmlabra@gmail.com>
2025-08-03 22:48:34 +0930

    fix(deploy): update deploy script to use main branch and handle remote origin
    refactor(projects): improve json formatting and update project details

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf build && bun run build && \
 cd build && git init && git add . \
 && git commit -m "Last build from react branch" && \
-git remote add origin https://github.com/matiaslabra/matiaslabra.github.io.git && \
-git push --force origin master && cd ..
+git remote add origin https://github.com/matiaslabra/matiaslabra.github.io.git 2>/dev/null || git remote set-url origin https://github.com/matiaslabra/matiaslabra.github.io.git && \
+git push --force origin HEAD:main && cd ..
